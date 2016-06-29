db949becec068c4e9ecbc3b35318591978faa0a2
Rathish <rathishvbr@outlook.com>
2016-06-29 19:23:10 +0530

    Region page.

--- app/controllers/region.js
+++ app/controllers/region.js
@@ -1,6 +1,25 @@
-import Ember from 'ember';
-export default Ember.Controller.extend({
+    import Ember from 'ember';
 
+    const {
+        Controller,
+        inject: {service},
+    } = Ember;
+
+
+export default Ember.Controller.extend({
+  regioninfos: service(),
+  storage: service(),
+  sessionStorage: service(),
+
+  hostInfos:   [ Ember.Object.create({
+    regionName: '',
+    compute: '',
+    storage: '',
+    }) ],
+
+  storeData(data, storage) {
+    return storage.setItem('megdc.hostinfos', JSON.stringify(data));
+  },
  actions: {
       stepThree() {
           alert('Thanks');
@@ -15,7 +34,35 @@ export default Ember.Controller.extend({
           this.toggleProperty('enabled');
               },
       close() {
-          this.toggleProperty('enabled');
+        //  this.toggleProperty('enabled');
+        alert(this.get('print'));
              },
+
+             addhost() {
+              // alert("1");
+                 this.get('hostInfos').pushObject(Ember.Object.create({
+                       regionName: '',
+                       compute: '',
+                       storage: '',
+                 }));
+            },
+
+
+            regionData() {
+
+              return this.get('ajax').request('/region/content', {
+                method: 'POST',
+                param:{
+                        regionName:this.get('print'),
+                        cost:
+                           {
+                             compute: this.get('compute'),
+                             storage: this.get('storage'),
+                           }
+
+                     }
+              });
+            },
+
             }
 });
